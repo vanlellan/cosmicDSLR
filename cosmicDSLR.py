4c24103a0d3fe06a02fc1c44e797b45f981309b6
Randall Evan McClellan <vanlellan@gmail.com>
2018-02-27 01:23:04 -0500

    added GPL, added blank readme

--- cosmicDSLR.py
+++ cosmicDSLR.py
@@ -1,4 +1,19 @@
+#cosmicDSLR: a program for searching for cosmic ray traces in digital camera images
+#Copyright 2018 Randall Evan McClellan
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 #
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 # ToDo
 #DONE	-pickle sum, sum2, and num instead of mean and mean2 (and save as just one picklejar)
 #DONE	-store everything in source directory by default (create new cosmic-search directory within source dir), with option to store in a different directory
